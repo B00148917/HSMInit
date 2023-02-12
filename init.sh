788261f970a4c1fa5852dd14fb72ec35fbf55a72
Mark O'Neill <114908700+B00148917@users.noreply.github.com>
2023-02-12 10:55:35 +0000

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,23 @@
+cd ~
+mkdir M1
+cd M1
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git -y
+sudo apt install gh -y
+
+#Config GH
+gh auth login
+gh repo clone b00148917/MyHSM
+
+git config --global user.email "b00148917@mytudublin.ie"
+git config --global user.name "Markone"
+
+#VSCode
+sudo snap install --classic code
+
+#Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm go*
